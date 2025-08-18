01da3a902e71e2140e8e701b5efc20801f56714a
StadttaubenMettmann <stadttauben_mettmann@gmx.de>
2025-08-18 23:34:03 +0200

    Update news-data.js

--- news-data.js
+++ news-data.js
@@ -6,8 +6,9 @@ const newsItems = [
     category: "Projekte",
     title: "Offizielle Fütterung und zukünfiger Taubenschlag",
     summary: "Durch unsere Arbeit wurde seitens der Stadt Mettmann mittlerweile eine offizielle Fütterung genehmigt.",
-    content: "Durch unsere Arbeit wurde seitens der Stadt Mettmann mittlerweile eine offizielle Fütterung genehmigt. Diese findet an dem Ort statt, an welchem in Zukunft auch ein Taubenschlag errichtet werden soll. Dieser ist die effektivste Lösung, um Tierschutz zu betreiben, in dem man den Tauben einen sicheren Rückzugsort mit artgerechtem Futter bietet, und gleichzeitig die Population eindämmen, in dem die Eier ausgetauscht werden. Die Tauben würden die Menschen nicht mehr  stören, da sie sich dann hauptsächlich in dem Schlag aufhalten und nicht mehr den ganzen Tag auf Nahrungssuche in der Nähe des Menschen sein müssen.",
+    content: "Durch unsere Arbeit wurde seitens der Stadt Mettmann mittlerweile eine offizielle Fütterung genehmigt. Diese findet an dem Ort statt, an welchem in Zukunft auch ein Taubenschlag errichtet werden soll. Dieser ist die effektivste Lösung, um Tierschutz zu betreiben, in dem man den Tauben einen sicheren Rückzugsort mit artgerechtem Futter bietet, und gleichzeitig die Population eindämmt, in dem die Eier ausgetauscht werden. Die Tauben würden die Menschen nicht mehr  stören, da sie sich dann hauptsächlich in dem Schlag aufhalten und nicht mehr den ganzen Tag auf Nahrungssuche in der Nähe des Menschen sein müssen.",
     image: "images/news-taubenschlag.jpg",
     tags: ["Taubenschlag", "Stadtpark", "Tierschutz"]
   }
 ];
+
